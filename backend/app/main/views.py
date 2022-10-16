584e4e5bc9a03c8a8ac181f797e511548c454ba3
Haki-Malai <hakimalaj@outlook.com>
2022-10-16 14:19:49 +0300

    Remove main routes and minor test module

--- backend/app/main/views.py
+++
@@ -1,9 +0,0 @@
-from . import main
-from .. import db
-from ..models import User
-from flask import session
-from datetime import datetime
-
-@main.route('/', methods=['GET'])
-def index():
-    return 'Hello World!'

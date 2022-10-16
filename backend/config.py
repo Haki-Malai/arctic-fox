4ae01a5db999ecbd737b64c732955a46174fa2c8
Haki-Malai <hakimalaj@outlook.com>
2022-10-16 14:13:18 +0300

    Config update

--- backend/config.py
+++ backend/config.py
@@ -3,35 +3,55 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     # Flask
-    UPLOAD_FOLDER = os.path.join(basedir, 'static/uploads')
-    # Session (More info: https://pythonhosted.org/Flask-Session/#configuration)
     SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "default-secret-key")
-    SESSION_TYPE = "redis"
-    # Database
+    UPLOAD_FOLDER = os.path.join(basedir, 'static/uploads')
+    # SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # WTF Forms
+    WTF_CSRF_ENABLED = False
+    # Custom
+    ADMIN = os.environ.get("ADMIN", "admin@arctic-fox.com")
+    POSTS_PER_PAGE = 20
+    COMMENTS_PER_PAGE = 30
+    FOLLOWERS_PER_PAGE = 50
+    # Flask-Mail
+    MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.googlemail.com")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT", "587"))
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "true").lower() in \
+        ["true", "on", "1"]
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_SUBJECT_PREFIX = '[Arctic Fox]'
+    MAIL_SENDER = f'Arctic Fox Admin <{ADMIN}>'
+    SLOW_DB_QUERY_TIME = 0.5
 
     @staticmethod
     def init_app(app):
         pass
 
 
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "data.sqlite"))
-    DEBUG = False
-    
-
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL", "sqlite:///" + os.path.join(basedir, "dev-data.sqlite"))
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL", \
+        "sqlite:///" + os.path.join(basedir, "dev-data.sqlite"))
     SESSION_COOKIE_SECURE = False
     SESSION_PERMAMENT = True
     
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL", "sqlite:///" + os.path.join(basedir, "test-data.sqlite"))
-    SESSION_COOKIE_SECURE = False
-    SESSION_PERMAMENT = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL", \
+        "sqlite:///" + os.path.join(basedir, "test-data.sqlite"))
+    DELETE_DB = True
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", \
+        "sqlite:///" + os.path.join(basedir, "data.sqlite"))
+    DELETE_DB = True
+    SQLALCHEMY_RECORD_QUERIES = True
+    SLOW_DB_QUERY_TIME = 0.5
+    SESSION_PERMAMENT = False
     
 
 config = {

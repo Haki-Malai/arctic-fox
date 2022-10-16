b743caa363d83033904d57e541f42e7f212ceefb
Haki-Malai <hakimalaj@outlook.com>
2022-10-16 14:15:27 +0300

    Reorganize Blueprints and trying out functions

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,25 @@
 from flask import Flask
+from flask_mail import Mail
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
+mail = Mail()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    db.init_app(app)
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    db.init_app(app)
+    mail.init_app(app)
+    login_manager.init_app(app)
+
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
 
     return app

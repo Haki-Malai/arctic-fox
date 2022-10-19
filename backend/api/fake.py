2f93f006fbc343f9a7eb61ba5881947ccf0e9bec
Haki-Malai <hakimalaj@outlook.com>
2022-10-19 14:22:45 +0300

    Fake data functions added

--- backend/api/fake.py
+++ backend/api/fake.py
@@ -2,15 +2,35 @@ from random import randint
 from sqlalchemy.exc import IntegrityError
 from faker import Faker
 from . import db
-from .models import User, Post, Comment
+from .models import User, Post, Follow, Comment, Notification, Task, Assignment
 from flask import current_app
+
+
+def fake_admins(count=2):
+    fake = Faker()
+    print('Generating fake admins...')
+    for i in range(count):
+        u = User(
+            email=fake.email(),
+            username=fake.user_name(),
+            password='password',
+            confirmed=True,
+            name=fake.name(),
+            location=fake.city(),
+            member_since=fake.past_date(),
+            role='admin'
+        )
+        db.session.add(u)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
 
 
 def fake_users(count=10):
     fake = Faker()
-    i = 0
     print('Generating fake users...')
-    while i < count:
+    for i in range(count):
         u = User(
             email=fake.email(),
             username=fake.user_name(),
@@ -27,6 +47,25 @@ def fake_users(count=10):
         except IntegrityError:
             db.session.rollback()
 
+            
+def fake_follows(count=100):
+    fake = Faker()
+    user_count = User.query.count()
+    print('Generating fake follows...')
+    for i in range(count):
+        u1 = User.query.offset(randint(0, user_count - 1)).first()
+        u2 = User.query.offset(randint(0, user_count - 1)).first()
+        f = Follow(
+            follower=u1.id,
+            followed=u2.id
+        )
+        db.session.add(f)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+
 def fake_posts(count=20):
     fake = Faker()
     user_count = User.query.count()
@@ -41,9 +80,9 @@ def fake_posts(count=20):
         db.session.add(p)
         try:
             db.session.commit()
-            i += 1
         except IntegrityError:
             db.session.rollback()
+
 
 def fake_comments(count=50):
     fake = Faker()
@@ -60,4 +99,45 @@ def fake_comments(count=50):
             post_id=p.id
         )
         db.session.add(c)
-    db.session.commit()
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+
+def fake_notifications(count=50):
+    fake = Faker()
+    user_count = User.query.count()
+    print('Generating fake notifications...')
+    for i in range(count):
+        u = User.query.offset(randint(0, user_count - 1)).first()
+        n = Notificatin.query.offset(randint(0, post_count - 1)).first()
+        db.session.add(n)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+    
+def fake_tasks(count=50):
+    fake = Faker()
+    user_count = User.query.filter_by(role='admin').count()
+    print('Generating fake tasks...')
+    for i in range(count):
+        assignee = User.query.offset(randint(0, user_count - 1)).first()
+        assigned = User.query.offset(randint(0, user_count - 1)).first()
+        t = Task(
+            name=fake.text(),
+            timestamp=fake.past_date(),
+            due_date=fake.future_date(),
+        )
+        a = Assignment(
+            task=t.id,
+            assignee=assignee.id,
+            assigned=assigned.id,
+        )
+        db.session.add(a,t)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()

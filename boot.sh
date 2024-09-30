f1df2518564177af55e26c746e3d3494ae0b4faf
Haki-Malai <hakimalai@outlook.com>
2024-09-30 18:25:07 +0300

    🔧 (boot.sh): update gunicorn command to point to the correct app module name "app"

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #flask db upgrade
-exec gunicorn --timeout 240 -b :80 --access-logfile /var/log/gunicorn/access.log --error-logfile /var/log/gunicorn/error.log arctic_fox:app
+exec gunicorn --timeout 240 -b :80 --access-logfile /var/log/gunicorn/access.log --error-logfile /var/log/gunicorn/error.log app:app
